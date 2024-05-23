d462d3e17f8cb6e141498d7872186c43c72d0da3
Tomsh666 <tomshinskiy02@gmail.com>
2024-05-23 14:34:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    print("1.Create signature")
+    print("2.Verify signature")
+    choice = input("Select an option:")
+    if choice == "1":
+        print("Done")
+    elif choice == "2":
+        print("Done")
+    else:
+        print("Wrong option")
+
+
+if __name__ == '__main__':
+    main()
